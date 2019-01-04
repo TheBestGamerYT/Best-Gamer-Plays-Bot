e8f8337e72a5068291447be131212415fa3e0cd3
TheBestGamerYT <franci641@gmail.com>
2019-01-04 20:44:33 +0100

    Add files via upload

--- bot.js
+++ bot.js
@@ -5,7 +5,7 @@ var prefix = "b! "
 
 client.on('ready',() => {
   console.log("I\'m Online !");
-  client.user.setActivity('b! is prefix', { type: 'PLAYING' })
+  client.user.setActivity('b! help | discord.gg/urSjkJx', { type: 'PLAYING' })
     .then(presence => console.log(`Activity set to ${presence.game ? presence.game.name : 'b! is prefix'}`))
     .catch(console.error);
 })
@@ -16,7 +16,31 @@ client.on('message', message => {
   		message.channel.send('pong');
   	}
     if (message.content.startsWith(prefix + 'help')) {
-  		message.channel.send('Welcome to a help page ! \n b! ping - pong \n b! help - help page');
+  		message.channel.send({embed:{
+  "plainText": "Welcome this is **Best Gamer Plays** Bots Help Page !",
+  "title": "Commands:",
+  "author": {
+    "name": "Click here to invite me !",
+    "url": "https://discordapp.com/oauth2/authorize?client_id=363356945760911360&scope=bot&permissions=8",
+    "icon_url": "https://pbs.twimg.com/profile_images/973406721011015680/GLCTdHA__400x400.jpg"
+  },
+  "color": 13632027,
+  "footer": {
+    "text": "|Made by TheBestGamerYT|-|Powered by TeamCo7 Developments|-|© 2018-2019|"
+  },
+  "fields": [
+    {
+      "name": "Misc:",
+      "value": "• b! ping - Pong !",
+      "inline": true
+    },
+    {
+      "name": "Utility:",
+      "value": "• b! help - Full list of commands",
+      "inline": true
+    }
+  ]
+}});
   	}
   });
 client.login(process.env.BOT_TOKEN);
